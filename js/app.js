76a2ab42e277495ba1dbebfd747c033fe48ebe32
Brahim <lenovo@LAPTOP-P20OVACF.com>
2021-02-06 22:18:32 +0100

    new demo

--- js/app.js
+++ js/app.js
@@ -1,30 +1,3 @@
-const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li");
-
-  burger.addEventListener("click", () => {
-    // Toggle Nav
-    nav.classList.toggle("nav-active");
-
-    //animate Links
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${
-          index / 7 + 0.5
-        }s`;
-      }
-    });
-    burger.classList.toggle("toggle");
-  });
-};
 function imgSlider(anything) {
   document.querySelector(".iwatch").src = anything;
 }
-function changeCircleColor(color) {
-  const circle = document.querySelector(".circel");
-  circle.style.background = color;
-}
-navSlide();
